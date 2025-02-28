78dcd7a458b5c39c422d6d4409f0eb8d2ba33172
kimberly <maldakimberly228@gmail.com>
2025-02-28 15:45:21 +0300

    creates a file

---
+++ index.js
@@ -0,0 +1,34 @@
+/*function shout(string){
+    return string.toUpperCase();
+}*/
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  const string = "Hello!";
+
+console.log(string);
+console.log(string.toUpperCase());
+
+console.log(string === string.toUpperCase());
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    }
+  }
+  console.log(sayHiToHeadphonedRoommate("hello")); 
+console.log(sayHiToHeadphonedRoommate("HELLO")); 
+console.log(sayHiToHeadphonedRoommate("Let's have dinner together!")); 
+console.log(sayHiToHeadphonedRoommate("Hey There!")); 
